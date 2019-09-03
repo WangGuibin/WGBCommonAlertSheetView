191db4f89d49ff27b27bd87abbefd53b59b076e9
YGC <guchengye153@gmail.com>
2019-09-03 15:42:33 +0700

    init

--- Example/WGBCommonAlertSheetView/WGBViewController.m
+++ Example/WGBCommonAlertSheetView/WGBViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "WGBViewController.h"
+#import "WGBCommonAlertSheetView.h"
 
 @interface WGBViewController ()
 
@@ -24,6 +25,27 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    CGRect bounds = UIScreen.mainScreen.bounds;
+    UITableView *tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, bounds.size.width, 500) style:(UITableViewStylePlain)];
+    
+    tableView.backgroundColor = [UIColor colorWithRed:arc4random()%256/255.0f green:arc4random()%256/255.0f  blue:arc4random()%256/255.0f alpha:1.0f];
+    
+    WGBCommonAlertSheetView *sheet = [[WGBCommonAlertSheetView alloc] initWithFrame:UIScreen.mainScreen.bounds containerView:tableView];
+    sheet.isNeedBlur = arc4random()%2;
+    sheet.blurStyle = arc4random()%2+1;
+    sheet.touchDismiss = YES;
+    [sheet show];
+    
+    [sheet setShowCompletionBlock:^{
+        NSLog(@"视图弹出来了");
+    }];
+    
+    [sheet setDismissCompletionBlock:^{
+        NSLog(@"视图消失了");
+    }];
 }
 
 @end
---
+++ WGBCommonAlertSheetView/Classes/WGBCommonAlertSheetView.h
@@ -0,0 +1,69 @@
+//
+//  WGBCommonAlertSheetView.h
+//  DY-ios
+//
+//  Created by mac on 2019/9/2.
+//  Copyright © 2019 CoderWGB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+typedef NS_ENUM(NSInteger, WGBCommonAlertSheetViewBlurEffectStyle) {
+    WGBCommonAlertSheetViewBlurEffectStyleLight = 1,
+    WGBCommonAlertSheetViewBlurEffectStyleDark
+} ;
+
+
+NS_ASSUME_NONNULL_BEGIN
+
+/**
+ Usage:
+ ```objc
+ 
+     UITableView *tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, screenWidth, 500) style:(UITableViewStylePlain)];
+     tableView.backgroundColor = randomColor;
+     WGBCommonAlertSheetView *sheet = [[WGBCommonAlertSheetView alloc] initWithFrame:UIScreen.mainScreen.bounds containerView:tableView];
+     sheet.isNeedBlur = arc4random()%2;
+     sheet.blurStyle = arc4random()%2+1;
+     sheet.touchDismiss = YES;
+     [sheet show];
+ ```
+ */
+
+@interface WGBCommonAlertSheetView : UIView
+
+- (instancetype)init NS_UNAVAILABLE;
+- (instancetype)initWithFrame:(CGRect)frame NS_UNAVAILABLE;
+- (instancetype)new NS_UNAVAILABLE;
+
+/**
+ ///MARK:- 唯一指定创建构造器
+
+ @param frame 一般情况设置全屏就完事了
+ @param containerView 具有frame的容器视图
+ @return 还你一个类似抖音评论的触底反弹的视图
+ */
+- (instancetype)initWithFrame:(CGRect)frame containerView:(UIView *)containerView;
+
+// 蒙层的透明度 默认0.35
+@property (nonatomic,assign) CGFloat maskAlpha;
+//触摸蒙层dismiss 默认为YES
+@property (nonatomic,assign) BOOL touchDismiss;
+
+//是否需要毛玻璃 默认是NO
+@property (nonatomic,assign) BOOL isNeedBlur;
+//毛玻璃的样式 这个必须设置不然没有任何效果 前提是`isNeedBlur=YES`
+@property (nonatomic,assign) WGBCommonAlertSheetViewBlurEffectStyle blurStyle;
+
+//展示到指定父视图上 superView为空即默认为放在主窗口keywindow上
+- (void)showAlertWithSuperView:(UIView * _Nullable)superView ;
+- (void)show;
+- (void)dismiss;
+
+@property (nonatomic,copy) dispatch_block_t showCompletionBlock; //展示之后
+@property (nonatomic,copy) dispatch_block_t dismissCompletionBlock;//消失之后
+
+
+@end
+
+NS_ASSUME_NONNULL_END
