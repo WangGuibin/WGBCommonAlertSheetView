69050780481a24cfbe2b5711476dee032f69ddeb
Wangguibin <864562082@qq.com>
2020-09-29 10:20:17 +0800

    更新pod

--- upload.sh
+++ upload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add .
-git commit -am  "1.0.1"
-git tag  1.0.1
+git commit -am  "1.0.2"
+git tag  1.0.2
 git push origin master --tags
 pod trunk push ./WGBCommonAlertSheetView.podspec --allow-warnings --verbose
